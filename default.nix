bf837c7a42762b725855b8b39eef50066b5eaf1a
PureFunctor <purefunctor@gmail.com>
2021-01-16 13:12:45 +0800

    Add initial Nix expressions for Nix-based builds

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ mkDerivation, base, stdenv }:
+mkDerivation {
+  pname = "haskell-project-boilerplate";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ base ];
+  executableHaskellDepends = [ base ];
+  testHaskellDepends = [ base ];
+  homepage = "https://github.com/PureFunctor/haskell-project-boilerplate";
+  description = "Boilerplate repository for setting up Haskell projects using Cabal and Nix with support for HLS";
+  license = stdenv.lib.licenses.bsd3;
+}
