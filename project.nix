70dbb943588aab8c20ac1ed2b9def248bd384cb2
PureFunctor <purefunctor@gmail.com>
2021-01-17 12:58:55 +0800

    Add aeson to dependencies

--- project.nix
+++ project.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, aeson, base, stdenv }:
 mkDerivation {
   pname = "haskell-project-boilerplate";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [ aeson base ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [ base ];
   homepage = "https://github.com/PureFunctor/haskell-project-boilerplate";
